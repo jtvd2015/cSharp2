e1174eddabedf4de510b4ec38094cee2abc6dbf0
Jennifer Wesselman <wesselman.j@gmail.com>
2018-02-07 14:14:01 -0500

    final commit, add A=65 comment to code

--- Assignment_char_i.cs
+++ Assignment_char_i.cs
@@ -6,7 +6,9 @@ namespace ConsoleApp6
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            //A=65
+
             for (int i = 65; i < 91; i++)
                 {
                 Console.WriteLine((char)i);
