fccdfa5158877dc33c01afbafa245856bafd7654
Jennifer Wesselman <wesselman.j@gmail.com>
2018-02-07 14:11:02 -0500

    first commit, make the Console write in A-Z using char(i)

---
+++ Assignment_char_i.cs
@@ -0,0 +1,21 @@
+using System;
+//using System.Text;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 65; i < 91; i++)
+                {
+                Console.WriteLine((char)i);
+                }
+            {
+
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
