e0afe398f95ad6117017f3aab9aa8a45f1039f12
Jennifer Wesselman <wesselman.j@gmail.com>
2018-02-08 14:13:11 -0500

    first commit

---
+++ addNumbers.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class fred
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 12, 35, 2, 17, 100, 54 };
+            int sum = numbers.Sum();
+
+            Console.WriteLine(sum);
+
+           
+            Console.ReadLine();
+        }
+    }
+}
